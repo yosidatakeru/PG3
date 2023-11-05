32220362ff08ebdfd61f3356d9c05e455c529e77
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2023-11-06 04:01:00 +0900

    仮

---
+++ PG3_02_02/Min.h
@@ -0,0 +1,20 @@
+#pragma once
+template <typename T1>
+
+class Min
+{
+public:
+	T1 height;
+	T1 width;
+
+	//コンストラクタ
+	 Min(T1 height, T1 width) :  height(height), width(width){}
+    
+	// メンバ関数
+	T1 Size()
+	{
+		return static_cast<T1>(height + width);
+	}
+
+};
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-﻿#include"Function/Function.h"
-#include"Enemy/Enemy.h"
+﻿#include"PG3_02_02/Min.h"
+#include<stdio.h>
 int main()
-{
-   
-
+{ 
+	Min<int>b1(10, 20);
+	Min<float>b2(1.2, 3.4);
+	
+	printf("b1=%d\n", b1.Size());
+	printf("b2=%f\n", b2.Size());
+	return 0;
 }
