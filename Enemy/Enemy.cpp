ed0c6b88a9f2f2b01675510c6042774f9dcb57b0
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2024-01-23 11:15:34 +0900

    完成

--- Enemy/Enemy.cpp
+++ Enemy/Enemy.cpp
@@ -22,13 +22,13 @@ void (Enemy::* Enemy::spFuncTadle[])() = {
 void Enemy::Attack()
 {
 	printf("Attack\n");
-	phase_ = Enemy::Shoots;
+	
 }
 
 void Enemy::Shoot()
 {
 	printf("Shoot\n");
-	phase_ = Enemy::Withdrawals;
+
 
 	
 }
@@ -47,7 +47,7 @@ void Enemy::Update()
 	
 	for (int i = 0; i < 3; i++)
 	{
-		(this->*spFuncTadle[static_cast<size_t>(phase_)])();
+		(this->*spFuncTadle[i])();
 	}
 	
 	
