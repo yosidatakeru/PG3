09227c438b4adb302d003d8fe0c47885ce7c6557
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2024-02-02 10:27:25 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
-﻿#include"PG3_02_02/Min.h"
+﻿#include"Min.h"
 #include<stdio.h>
 int main()
 { 
 	Min<int,int>comperVal1(10, 20);
 	Min<int,float>comperVal2(12, 3.4f);
-	Min<int,double>comperVal3(10.3, 10.4);
+	Min<int,double>comperVal3(10.8, 10.4);
+	Min<float, float>comperVal4(21.0f, 15.0f);
+	Min<float, double>comperVal5(23.0f, 57.0);
+	Min<double, double>comperVal6(26.0, 63.0);
 
-	Min<float, float>comperVal4(20.0f, 15.0f);
-	Min<float, double>comperVal5(20.0f, 57.0);
-
-	Min<double, double>comperVal6(25.0, 63.0);
 	printf("b1=%d\n", comperVal1.comparison());
 	printf("b2=%d\n", comperVal2.comparison());
 	printf("b2=%d\n", comperVal3.comparison());
