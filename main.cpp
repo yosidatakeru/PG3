d63c7abcdd3b8e2d9b803c4d920b127a91b310a0
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2024-02-09 11:10:18 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,28 +1,73 @@
 ﻿#include<stdio.h>
-#include"IShape.h"
-#include"Circle.h"
-#include"Rectangle.h"
+#include<list>
+#include<iostream>
+
+using namespace std;
 int main()
 {
-	static const int AMOUNT = 2;
-	IShape* rectangle[AMOUNT] = { nullptr };
-
-	rectangle[0] = new Circle(2.0f);
-	rectangle[1] = new Rectangle(15.0f, 25.0f);
-
-	for (int i = 0; i < AMOUNT; i++)
+	std::list<const char*> yamanoteseStation
 	{
-		rectangle[i]->Size();
+		"Tokyo","Kanda","Akihabara","Okachimachi","Ueno","Uguisudani","Nippori","Tabata","Komagome",
+		"Sugamo","Otsuka","Ikebukuro","Mejiro","Takadanobaba","Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
+		"Ebisu","Meguro","Gotanda","Osaki","Shinagawa","Tamachi","Hamamatsucho","Shimbashi","Yurakucyo"
+	};
+
+	cout << "山手線の全駅" << "\n";
+	cout << "1970年、2019年、2022年の一覧を表示します" << "\n";
+	cout << "どの年を表示しますか" << "\n";
+
+	//入力
+	int selectYear = 0;
+	cin >> selectYear;
+
+	if (selectYear == 1970)
+	{
+		cout << "1970年の駅一覧を表示します" << "\n";
 	}
 
-	for (int i = 0; i < AMOUNT; i++)
+	if (selectYear == 2019)
 	{
-		rectangle[i]->Draw();
+		cout << "2019年の駅一覧を表示します" << "\n";
+		for (list<const char*>::iterator itr = yamanoteseStation.begin(); itr != yamanoteseStation.end(); ++itr)
+		{
+			if (*itr == "Tadata")
+			{
+				itr = yamanoteseStation.insert(itr, "Nish-Nippori");
+				++itr;
+			}
+
+			
+
+		}
+
 	}
 
-	for (int i = 0; i < AMOUNT; i++)
+	if (selectYear == 2022)
 	{
-		delete rectangle[i];
+		cout << "2022年の駅一覧を表示します" << "\n";
+		for (list<const char*>::iterator itr = yamanoteseStation.begin(); itr != yamanoteseStation.end(); ++itr)
+		{
+			if (*itr == "Tadata")
+			{
+				itr = yamanoteseStation.insert(itr, "Nish-Nippori");
+				++itr;
+			}
+
+			if (*itr == "Tamachi")
+			{
+				itr == yamanoteseStation.insert(itr, "Takanawa GateWay");
+				++itr;
+			}
+
+		}
+
 	}
-		return 0;
+	for (list<const char*>::iterator itr = yamanoteseStation.begin(); itr != yamanoteseStation.end(); ++itr) {
+
+		cout << *itr << "\n";
+	}
+
+	
+
+	return 0;
 }
