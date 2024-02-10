ad7068c693167d016dfca07f70b04b66bd703d17
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2024-02-10 22:32:38 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,23 +1,36 @@
-﻿#include<thread>
-#include<vector>
+﻿#include <stdio.h>
 #include <string>
-#include"Calation.h"
-#include"DisplayText.h"
+#include <chrono>
+#include <iostream>
+
 
 int main()
 {
-	std::vector<std::string>threadName{ "thread 1","thread 2","thread 3" };
 
-	std::thread th1(DisplayText, threadName[0]);
-	th1.join();
-
-	std::thread th2(DisplayText, threadName[1]);
-	th2.join();
-
-	std::thread th3(DisplayText, threadName[2]);
-	th3.join();
+	const int TEXT_AMOUNT = 100000;
+	std::string a(TEXT_AMOUNT, 'a');
+	std::cout << TEXT_AMOUNT << "文字のコピーと移動を比較\n";
 
 
+
+	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+	std::string a2 = a;
+
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+	auto result = std::chrono::duration_cast<std::chrono::microseconds>(end - now).count();
+
+	std::cout << "コピーの場合" << "\n";
+	std::cout << result << "\n";
+
+	std::chrono::steady_clock::time_point now2 = std::chrono::steady_clock::now();
+	std::string a3 = std::move(a);
+
+	std::chrono::steady_clock::time_point end2 = std::chrono::steady_clock::now();
+	auto result2 = std::chrono::duration_cast <std::chrono::microseconds> (end2 - now2).count();
+
+	std::cout << "移動の場合" << "\n";
+	std::cout << result2 << "\n";
 
 	return 0;
 }
